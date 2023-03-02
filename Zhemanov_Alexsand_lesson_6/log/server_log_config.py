9e646b7512c0bf1de0208333e18622b8fb527bfa
vios <i.svoboda.vios@gmail.com>
2023-03-02 16:21:27 +0300

    copy from other branch

---
+++ Zhemanov_Alexsand_lesson_6/log/server_log_config.py
@@ -0,0 +1,51 @@
+"""
+SERVER
+Для проекта «Мессенджер» реализовать логирование с использованием модуля logging:
+
+1. В директории проекта создать каталог log, в котором для клиентской и серверной сторон в отдельных модулях формата client_log_config.py и server_log_config.py создать логгеры;
+2. В каждом модуле выполнить настройку соответствующего логгера по следующему алгоритму:
+Создание именованного логгера;
+Сообщения лога должны иметь следующий формат: "<дата-время> <уровеньважности> <имямодуля> <сообщение>";
+Журналирование должно производиться в лог-файл;
+На стороне сервера необходимо настроить ежедневную ротацию лог-файлов.
+
+3. Реализовать применение созданных логгеров для решения двух задач:
+Журналирование обработки исключений try/except. Вместо функции print() использовать журналирование и обеспечить вывод служебных сообщений в лог-файл;
+Журналирование функций, исполняемых на серверной и клиентской сторонах при работе мессенджера.
+"""
+import logging.handlers
+import logging
+
+LOGGER_TERMINAL_LEVEL = logging.DEBUG
+LOGGER_FILE_LEVEL = logging.ERROR
+
+# open all log
+logging.root.setLevel(logging.NOTSET)
+logger = logging.getLogger("server")
+
+# format
+logger_format = logging.Formatter(
+    "<%(asctime)s> <%(levelname)s> <%(name)s> <%(message)s>")
+# file
+logger_file = logging.handlers.TimedRotatingFileHandler("server.log",
+                                                        "D",
+                                                        1,
+                                                        encoding="utf-8")
+logger_file.setLevel(LOGGER_FILE_LEVEL)
+# terminal
+logger_terminal = logging.StreamHandler()
+logger_terminal.setLevel(LOGGER_TERMINAL_LEVEL)
+# apply Formatter
+logger_file.setFormatter(logger_format)
+logger_terminal.setFormatter(logger_format)
+
+# apply all handler
+logger.addHandler(logger_file)
+logger.addHandler(logger_terminal)
+
+if __name__ == "__main__":
+    logger.debug("debug text")
+    logger.info("info text")
+    logger.warning("warning text")
+    logger.error("error text")
+    logger.critical("critical text")
