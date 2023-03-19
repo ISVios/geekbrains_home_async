dc1a207eaec5cdf304c9fc3c40c074b931ce83b3
vios <i.svoboda.vios@gmail.com>
2023-03-20 00:50:35 +0300

    alpha tasks (1-3)

--- Zhemanov_Alexsand_lesson_9/task_1.py
+++ Zhemanov_Alexsand_lesson_9/task_1.py
@@ -7,63 +7,61 @@
     В функции необходимо перебирать ip-адреса и проверять их доступность
         с выводом соответствующего сообщения («Узел доступен», «Узел недоступен»).
     При этом ip-адрес сетевого узла должен создаваться с помощью функции ip_address().
-2. Написать функцию host_range_ping() для перебора ip-адресов из заданного диапазона.
-    Меняться должен только последний октет каждого адреса.
-    По результатам проверки должно выводиться соответствующее сообщение.
-3. Написать функцию host_range_ping_tab(),
-        возможности которой основаны на функции из примера 2.
-    Но в данном случае результат должен быть итоговым по всем ip-адресам,
-        представленным в табличном формате (использовать модуль tabulate).
-        Таблица должна состоять из двух колонок и выглядеть примерно так:
-            Reachable Unreachable
-            10.0.0.1  10.0.0.3
-            10.0.0.2  10.0.0.4
 """
 
 import ipaddress
-import logging
-import os
 import subprocess
 from socket import gethostbyname
 
-import tabulate
+import logger
 
-logger = logging.getLogger(__name__)
-logging.root.setLevel(0)
-logger.setLevel(0)
-logger.addHandler(logging.StreamHandler())
+LOGGER = logger.LOGGER
 
 
-def ip_address(host: str):
+def get_ips(host: str):
+    """
+    return correct ip list address from: 
+        (example)   '192.168.0.1'       ->  [192.168.0.1]
+        (example)   'google.com'        ->  [64.233.165.113]
+        (example)   '192.168.0.0/24'    ->  [192.168.0.1 ... 192.168.0.255]
+        (example)   '192.168.02222.222' ->  [None]
+    """
     try:
-        ip = ipaddress.ip_address(host)
+        return [ipaddress.ip_address(host)]
     except:
         try:
-            ip = ipaddress.ip_address(gethostbyname(host))
+            return [ipaddress.ip_address(gethostbyname(host))]
         except:
-            ip = None
+            try:
+                return list(ipaddress.ip_network(host).hosts())
+            except:
+                return None
 
-    return ip
 
-
-def host_ping(hosts: list[str] = []):
+def host_ping(hosts: list[str] = [], print_effect: bool = True) -> bool:
 
     ARGS = ["/usr/bin/ping", "-c4"]
 
     for host in hosts:
-        ip = ip_address(host)
-        res = subprocess.Popen(args=[*ARGS, str(ip)], stdout=subprocess.PIPE)
-        res.wait()
-        status = "Reachable" if res.returncode == 0 else "Unreachable"
-        logger.info(f"{host} {status}")
+        ips = get_ips(host)
+
+        if not ips:
+            LOGGER.error(f"Invalid address '{host}'")
+            # Think: exit or continue
+            exit(-1)
+        for ip in ips:
+            res = subprocess.Popen(args=[*ARGS, str(ip)],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.DEVNULL)
+            res.wait()
+            ok = res.returncode == 0
+            status = "Reachable" if ok else "Unreachable"
+            if print_effect:
+                LOGGER.info(f"{host} {status}")
+            return ok
+    return False
 
 
-def host_range_ping():
-    pass
-
-
-def host_range_ping_tab():
-    pass
-
-
-host_ping(["192.168.0.1"])
+if __name__ == "__main__":
+    TEST_ADDR = ["192.168.0.1", "127.0.0.1", "yandex.ru", "google.com"]
+    host_ping(TEST_ADDR)
